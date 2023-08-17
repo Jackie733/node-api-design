742a8414ce4b18920798908b1c7e887008e1a29b
Jackie Tsien <jackie10673311@gmail.com>
2023-08-18 06:22:13 +0800

    init commit

---
+++ src/router.ts
@@ -0,0 +1,61 @@
+import { Router } from 'express';
+import { body, oneOf, validationResult } from 'express-validator';
+import { handleInputErrors } from './modules/middleware';
+
+const router = Router();
+
+/**
+ * Product
+ */
+router.get('/product', (req, res) => {
+  res.json({message:req.requestTime});
+})
+router.get('/product/:id', () => {})
+router.put('/product/:id', body('name').isString(), handleInputErrors, (req, res) => {
+  
+})
+router.post('/product/', body('name').isString(), handleInputErrors, () => {})
+router.delete('/product/:id', () => {})
+
+/**
+ * Update
+ */
+router.get('/update', () => {
+
+})
+router.get('/update/:id', () => {})
+router.put('/update/:id',
+  body('title').optional(),
+  body('body').optional(),
+  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+  body('version').optional(), 
+  () => {}
+)
+router.post('/update/',
+  body('title').exists().isString(),
+  body('body').exists().isString(),
+  () => {}
+)
+router.delete('/update/:id', () => {})
+
+/**
+ * Update Point
+ */
+router.get('/updatepoint', () => {
+
+})
+router.get('/updatepoint/:id', () => {})
+router.put('/updatepoint/:id',
+  body('name').optional().isString(),
+  body('description').optional().isString(),
+  () => {}
+)
+router.post('/updatepoint/',
+  body('name').optional().isString(),
+  body('description').optional().isString(),
+  body('updateId').exists().isString(),
+  () => {}
+)
+router.delete('/updatepoint/:id', () => {})
+
+export default router;
